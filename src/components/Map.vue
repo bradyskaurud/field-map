6602880f207ca6ba0b360f27775c06b90cb5843f
Brady Skaurud <bradyskaurud@gmail.com>
2018-04-07 10:09:09 -0500

    Initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>{{msg}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'map',
+    data () {
+        return {
+        msg: 'Welcome to Your Maps'
+        }
+    }
+}
+</script>
