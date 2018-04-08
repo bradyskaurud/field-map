749ca3503b40ffeeb2af8a645566e83abfa41735
Brady Skaurud <bradyskaurud@gmail.com>
2018-04-08 00:32:28 -0500

    [FEATURE] Generate google maps link

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -18,14 +18,16 @@ export default {
       // `this` points to the vm instance
       axios.get("http://localhost:3000/fields")
       .then((response)  =>  {
-        var map = L.map('mapid').setView([51.505, -0.09], 13);
+        var map = L.map('mapid').setView([47.379851, -96.262011], 10);
 
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        response.data.features.forEach((feature) => {
-          L.geoJSON(feature).addTo(map);
+        response.data.features.forEach((feature) => { 
+          const field = L.geoJSON(feature).addTo(map);
+          const mapUrl = `https://www.google.com/maps/dir/?api=1&origin=&destination=${feature.properties.Latitude},${feature.properties.Longitude}&travelmode=driving`;
+          field.bindPopup(`${feature.properties.COMNAME} <a href="${mapUrl}" target="_blank"/> GO HERE </button>`);
         });
       }, (error)  =>  {
         throw new Error(error);
@@ -35,7 +37,15 @@ export default {
 </script>
 
 <style>
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;  
+  }
+
   #mapid { 
-    height: 300px; 
+    height: 1000px;
+    width: 1000px; 
   }
 </style>
