6b989fedd39dc396788127afd1f007f943955ca5
Brady Skaurud <bradyskaurud@gmail.com>
2018-04-12 19:25:51 -0500

    [CHORE] Remoe api directory

--- api/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const fieldController = require('../controllers/field');
-
-router.get('/api/fields', fieldController.all);
-
-module.exports = router;
